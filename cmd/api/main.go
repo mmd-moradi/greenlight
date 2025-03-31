7db1f2483095e5e69f6f3cdd1bb992803808e191
mmd-moradi <mmdhk83@gmail.com>
2025-03-30 22:06:55 -0300

    feat: add CORS middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -66,6 +70,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "45ccabcc613427", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "2f7ecdfe510366", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@mmdmi.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "List of trusted CORS origins(space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
